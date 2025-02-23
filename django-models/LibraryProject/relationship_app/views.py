d3f8aeaf0f9bcdf08462bf47814b30cb5ccf7b57
JacklineMacharia <jacklinemacharia99@gmail.com>
2025-02-23 23:07:48 +0300

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.contrib.auth import login, logout, authenticate
+# register
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+
+# Login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+# logout
+from django.contrib.auth.views import LogoutView
+
 
 # Create your views here.
 def list_books(request):
@@ -21,21 +31,11 @@ def RegisterView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login') 
     template_name = 'relationship_app/register.html'
-   
-    
-def LoginView(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('list_books')
-        else:
-            form = AuthenticationForm()
-        return render(request, 'relationship_app/login.html', {'form':form})
-    
-def LogoutView(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('logout')
-    return render(request, 'relationship_app/logout.html')
+      
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+]
+
+urlpatterns += [  # Append logout URL to existing urlpatterns
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
