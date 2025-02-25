7f27379cdc5fd6294981ffdcd26a241de61f98da
JacklineMacharia <jacklinemacharia99@gmail.com>
2025-02-26 01:15:42 +0300

    Deep Dive into Django Models and Views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ from .views import list_books, LibraryDetailView
 
 from .views import admin_view, librarian_view, member_view
 
-from .views import add_book, edit_book, delete_book
+from .views import add_book, edit_book, delete_book, book_list
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -23,7 +23,8 @@ urlpatterns = [
 ]
 
 urlpatterns = [
-    path('add-book/', add_book, name='add_book'),
-    path('edit-book/<int:book_id>/', edit_book, name='edit_book'),
-    path('delete-book/<int:book_id>/', delete_book, name='delete_book'),
+    path('add_book/', add_book, name='add_book'),  # Route for adding a book
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),  # Route for editing a book
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),  # Route for deleting a book
+    path('books/', book_list, name='book_list'),  # Route to list books
 ]
