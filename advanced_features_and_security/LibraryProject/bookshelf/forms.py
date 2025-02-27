1cbd776671e0c9a4681ad50037c9f330e15a1663
JacklineMacharia <jacklinemacharia99@gmail.com>
2025-02-27 20:35:36 +0300

    ..

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import CustomUser
+
+class CustomUserForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already exists.")
+        return email
