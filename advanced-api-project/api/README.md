0e30d28a90312ab8a1c5ebbce48e4d560ff16f13
JacklineMacharia <jacklinemacharia99@gmail.com>
2025-03-13 22:36:36 +0300

    customizing Views

---
+++ advanced-api-project/api/README.md
@@ -0,0 +1,12 @@
+# Django REST API - Advanced API Project
+
+## Description
+This project is a Django REST API for managing books and authors using Django REST Framework (DRF).
+
+## API Endpoints
+- `GET /books/` - Retrieve all books (public)
+- `GET /books/<id>/` - Retrieve a single book (public)
+- `POST /books/create/` - Add a book (authenticated users only)
+- `PUT /books/<id>/update/` - Update a book (authenticated users only)
+- `DELETE /books/<id>/delete/` - Delete a book (authenticated users only)
+
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+# Retrieve all books
+"""
+    API view to retrieve all books.
+    Allows read-only access for unauthenticated users.
+"""
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] # Unauthenticated users can read only
+    
+# Retrieving a single book by id
+class BookDetailview(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+# Add a new book    
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated] # Only authenticated can create
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            publication_year = serializer.validated_data.get("publication_year")
+            if publication_year > 2025:
+                return Response({"error": "Publication year cannot be after 2025."}, status=status().HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# Modify an existing book
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+# Delete a book
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes =  [permissions.IsAuthenticated]
